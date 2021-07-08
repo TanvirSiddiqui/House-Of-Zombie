090d87ab0adf8796d76c08d6f58b0bddb96ac71c
Md. Tanvir Siddiqui <babatanvir@hotmail.com>
2021-07-08 13:52:03 +0600

    Triggering Animation In Code.

--- House of Zombie/Assets/Scripts/EnemyAI.cs
+++ House of Zombie/Assets/Scripts/EnemyAI.cs
@@ -45,10 +45,13 @@ public class EnemyAI : MonoBehaviour
     }
     private void AttackTarget()
     {
+        GetComponent<Animator>().SetBool("Attack",true);
         Debug.Log(name+" has eaten the "+target.name);
     }
     private void ChaseTarget()
     {
+        GetComponent<Animator>().SetBool("Attack", false);
+        GetComponent<Animator>().SetTrigger("Move");
         navMeshAgent.SetDestination(target.position);
     }
 

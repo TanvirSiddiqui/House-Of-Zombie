9bc14a5875c9a0170c1d9328f65041ae0d7547fa
Md. Tanvir Siddiqui <babatanvir@hotmail.com>
2021-08-01 00:39:34 +0600

    WeaponZoom -Mouse Sensitivity.

--- House of Zombie/Assets/Scripts/WeaponZoom.cs
+++ House of Zombie/Assets/Scripts/WeaponZoom.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
@@ -8,8 +9,16 @@ public class WeaponZoom : MonoBehaviour
     [SerializeField] float zoomOutFOV = 60f;
     [SerializeField] float zoomInFov = 20f;
 
+    [SerializeField] float zoomOutSensitivity =2f;
+    [SerializeField] float zoomInSensitivity = 0.5f;
     bool zoomedInToggle = false;
-    
+
+    RigidbodyFirstPersonController fpsController;
+
+     void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -19,11 +28,15 @@ public class WeaponZoom : MonoBehaviour
             {
                 zoomedInToggle = true;
                 fpsCamera.fieldOfView = zoomInFov;
+                fpsController.mouseLook.XSensitivity = zoomInSensitivity;
+                fpsController.mouseLook.YSensitivity= zoomInSensitivity;
             }
             else
             {
                 zoomedInToggle = false;
                 fpsCamera.fieldOfView = zoomOutFOV;
+                fpsController.mouseLook.XSensitivity = zoomOutSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomOutSensitivity;
             }
         }
     }

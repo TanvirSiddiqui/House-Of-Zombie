273b68a93f2af488fff51472f7b93b60a4eeb9ab
Md. Tanvir Siddiqui <babatanvir@hotmail.com>
2021-08-10 01:42:03 +0600

    Adding WeaponSwitcher.cs script

---
+++ House of Zombie/Assets/Scripts/WeaponSwitcher.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour
+{
+    [SerializeField] int currentWeapon = 0;
+    void Start()
+    {
+        SetWeapon();
+    }
+
+    private void SetWeapon()
+    {
+        int weaponIndex = 0;
+        foreach(Transform weapon in transform)
+        {
+            if (weaponIndex == currentWeapon)
+            {
+                weapon.gameObject.SetActive(true);
+            }
+            else
+            {
+                weapon.gameObject.SetActive(false);
+            }
+            weaponIndex++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

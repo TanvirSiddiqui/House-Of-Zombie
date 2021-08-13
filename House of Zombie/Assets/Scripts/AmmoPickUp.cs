e198f5784c867a1d293035869ad9f76df48b1a16
Md. Tanvir Siddiqui <babatanvir@hotmail.com>
2021-08-14 01:39:10 +0600

    Ammo PickUp(Part-2)

--- House of Zombie/Assets/Scripts/AmmoPickUp.cs
+++ House of Zombie/Assets/Scripts/AmmoPickUp.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class AmmoPickUp : MonoBehaviour
 {
+    [SerializeField] int ammoAmount = 5;
+    [SerializeField] AmmoType ammoType;
     private void OnTriggerEnter(Collider other)
     {
+        
         if (other.gameObject.tag == "Player")
         {
-            Debug.Log("Player took the Ammo");
+            FindObjectOfType<Ammo>().IncreaseCurrentAmmo(ammoType, ammoAmount);
             Destroy(gameObject);
         }
     }
